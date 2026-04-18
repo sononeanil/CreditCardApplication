48315488eed2b8195fa4f29602937ba27c463a7e
sononeanil <anilsonone@gmail.com>
2026-04-18 12:26:59 +0530

    Initial commit

---
+++ src/main/java/com/hcl/cc/ErpSystem2025Nov271Application.java
@@ -0,0 +1,13 @@
+package com.hcl.cc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErpSystem2025Nov271Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErpSystem2025Nov271Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/cc/ErpSystem2025Nov271ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcl.cc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErpSystem2025Nov271ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

53b38b2c472935c39f99a3fdcc53a61c98b1504a
adipat316 <adipat316@gmail.com>
2026-04-18 14:19:12 +0530

    Commit

---
+++ src/main/java/com/hcl/card/entity/EmploymentDetails.java
@@ -0,0 +1,24 @@
+package com.hcl.card.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Table(name = "employment_details")
+public class EmploymentDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long employmentId;
+    private String userId;
+    private String employerName;
+    private String designation;
+    private Integer numberOfExpYears;
+    private Double annualIncome;
+    private boolean currentEmployer;
+    
+    // Getters & Setters
+}

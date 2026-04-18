858d0d16f515f3f2f1e6232bce1a600a8483e729
adipat316 <adipat316@gmail.com>
2026-04-18 14:40:19 +0530

    Update EmploymentDetailsRepository.java

--- src/main/java/com/hcl/card/repository/EmploymentDetailsRepository.java
+++ src/main/java/com/hcl/card/repository/EmploymentDetailsRepository.java
@@ -6,6 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 @Repository
 public interface EmploymentDetailsRepository 
         extends JpaRepository<EmploymentDetails, String> {
-
-    List<EmploymentDetails> findByUserId(String userId);
 }

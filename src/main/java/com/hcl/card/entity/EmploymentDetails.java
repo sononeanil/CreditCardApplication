c8117949f66a0b1b299abdc95d87f82bfd0aa2ef
adipat316 <adipat316@gmail.com>
2026-04-18 14:40:46 +0530

    Update EmploymentDetails.java

--- src/main/java/com/hcl/card/entity/EmploymentDetails.java
+++ src/main/java/com/hcl/card/entity/EmploymentDetails.java
@@ -13,11 +13,22 @@ public class EmploymentDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long employmentId;
+    
+    @Column(name = "userid", nullable = false, length = 100)
     private String userId;
+    
+    @Column(name = "employerName", nullable = false, length = 100)
     private String employerName;
+    
+    @Column(name = "designation", nullable = false, length = 100)
     private String designation;
+    
+    @Column(name = "numberOfExpYears", nullable = false, length = 2)
     private Integer numberOfExpYears;
+    
+    @Column(name = "annualIncome", nullable = false, length = 10)
     private Double annualIncome;
+    
     private boolean currentEmployer;
     
     // Getters & Setters
